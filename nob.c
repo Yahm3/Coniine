b0bd2847297bb7afe964bd71372e7fd47816e4ad
Yahm3 <240418541@tut4life.ac.za>
2026-05-18 21:47:14 +0200

    test: add a flags to view macro variables while debugging

--- nob.c
+++ nob.c
@@ -14,6 +14,7 @@ int main(int argc, char **argv) {
   NOB_GO_REBUILD_URSELF(argc, argv);
   Nob_Cmd cmd = {0};
   nob_cmd_append(&cmd, "gcc");
+  nob_cmd_append(&cmd, "-g3");
   nob_cmd_append(&cmd, "-ggdb");
   nob_cmd_append(&cmd, "main.c");
   nob_cmd_append(&cmd, "-o");
